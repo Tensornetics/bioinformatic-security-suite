a2d63971d8ee25a5b567db2111e76c4a32e3128d
Kyle Grant <95764976+K-MkrOps@users.noreply.github.com>
2023-01-17 13:30:44 -0800

    Create blockchain.js
    In this example, the Blockchain class has several methods such as createGenesisBlock() which creates the first block of the chain, getLatestBlock() which returns the latest block in the chain, addBlock() which adds a new block to the chain, and isChainValid() which checks if the chain is valid by comparing the current block's hash and previousHash with their calculated values.
    
    The class also has a constructor which creates an empty chain and pushes the genesis block to it.
    
    This is a basic example of the implementation of a blockchain class, and the actual implementation will vary depending on the specific requirements and design of the project.

---
+++ src/blockchain/blockchain.js
@@ -0,0 +1,39 @@
+const Block = require('./block');
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "Genesis block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+module.exports = Blockchain;
